62851d8b69ab9a38b112aa2f25450e125686eb5e
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-11-25 17:37:28 +0100

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """The Base model class"""
 # import models
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
@@ -11,14 +11,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs) -> None:
         """Initalize a new base model"""
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.datetime.strptime
-                    (value, tform)
+                    self.__dict__[key] = datetime.strptime(value, tform)
                 else:
                     self.__dict__[key] = value
         # else:
@@ -26,7 +25,7 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute updated_at"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         # models.storage.new(self)
 
     def to_dict(self):
