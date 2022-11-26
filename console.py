c428d11e0b7556c9ea9ce9748a15f8be62be92bf
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-11-26 14:40:25 +0100

    commit

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ from models.user import User
 class HBNBCommand(cmd.Cmd):
     """Defines the airbnb clone interpreter"""
     intro = " Welcome to HBNB shell, Type help or ? to list commands.\n"
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
     __classes = {
         "BaseModel",
         "User",
@@ -120,8 +120,22 @@ class HBNBCommand(cmd.Cmd):
                 obj1.append(obj.__str__())
         print(obj1)
 
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        argl = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if argl[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
+
     def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
         argl = parse(arg)
         objdict = storage.all()
 
@@ -146,34 +160,25 @@ class HBNBCommand(cmd.Cmd):
             except NameError:
                 print("** value missing **")
                 return False
+
         if len(argl) == 4:
             obj = objdict["{}.{}".format(argl[0], argl[1])]
             if argl[2] in obj.__class__.__dict__.keys():
-                vtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argl[2]] = vtype(argl[3])
-            obj.__dict__[argl[2]] = argl[3]
-
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
+            else:
+                obj.__dict__[argl[2]] = argl[3]
         elif type(eval(argl[2])) == dict:
             obj = objdict["{}.{}".format(argl[0], argl[1])]
-            for k, value in eval(argl[2]).items():
+            for k, v in eval(argl[2]).items():
                 if (k in obj.__class__.__dict__.keys() and
                         type(obj.__class__.__dict__[k]) in {str, int, float}):
-                    vtype = type(obj.__class__.__dict__[k])
-                    obj.__dict__[k] = vtype(value)
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
                 else:
-                    obj.__dict__[k] = value
+                    obj.__dict__[k] = v
         storage.save()
-
-    def do_count(self, arg):
-        """Retrieve the number of instances of a given class.
-        Usage: <count name>.count()"""
-        argl = parse(arg)
-        count = 0
-        for obj in storage.all().values():
-            if argl[0] == obj.__class__.__name__:
-                count += 1
-        print(count)
-
+    
     def do_clear(self, arg):
         """Clears the screen"""
         os.system("cls")
